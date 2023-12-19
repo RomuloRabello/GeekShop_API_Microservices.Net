aaee1def864370aaa919931c2fffdd8d2c315da3
RomuloRabello <romulo.rabllo@gmail.com>
2023-12-19 20:16:54 -0300

    commit

---
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<IActionResult> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        public async Task<IActionResult> FindAll(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+    }
+}
