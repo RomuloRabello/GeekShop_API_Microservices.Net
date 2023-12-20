99aa9243d37a672e3ffde2503d6d73ecfe028fd2
RomuloRabello <romulo.rabllo@gmail.com>
2023-12-20 10:43:23 -0300

    Commit verbos API - Product

--- GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.ProductAPI.Repository;
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,18 +15,46 @@ namespace GeekShopping.ProductAPI.Controllers
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
 
-        public async Task<IActionResult> FindById(long id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
         {
             var product = await _repository.FindById(id);
             if (product == null) return NotFound();
             return Ok(product);
         }
 
-        public async Task<IActionResult> FindAll(long id)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
         {
-            var product = await _repository.FindById(id);
-            if (product == null) return NotFound();
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+
+            if (vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
             return Ok(product);
         }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+
+
     }
 }
